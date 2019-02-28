5363426d917450d8a4be961bd2b6be1d3e0768f3
ppecheux <pierrelouis.pecheux@gmail.com>
2019-02-28 20:58:22 +0100

    eval fonction

--- code.py
+++ code.py
@@ -1,14 +1,73 @@
-def get_data():
-    file_name = "a_example.txt"
-    file_obj = open(file_name, "r+")
-    lines = file_obj.read().split("\n")
-    N = int(lines[0])
-    lines = lines[1:]
-    imgs = {}
-    for i,v in enumerate(lines):
-        imgs[i] = 
-    print(lines)
+# def get_data():
+#     file_name = "a_example.txt"
+#     file_obj = open(file_name, "r+")
+#     lines = file_obj.read().split("\n")
+#     N = int(lines[0])
+#     lines = lines[1:]
+#     imgs = {}
+#     for i,v in enumerate(lines):
+#         imgs[i] = 
+#     print(lines)
 
-get_data()
+# get_data()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def transition_evaluation(tags1= [1,2,4],tags2 = [2,3,4]):
+    # common_tags= set(tags1).intersection(tags2)
+    # print(common_tags)
+    # unic_tags1= set(tags1).difference(tags2)
+    # print(unic_tags1)
+    # unic_tags2= set(tags2).difference(tags1)
+    # print(unic_tags2)
+    # score= min(len(common_tags),len(unic_tags1),len(unic_tags2))
+    # print (score)
+    return min(
+        len(set(tags1).intersection(tags2)),
+        len(set(tags1).difference(tags2)),
+        len(set(tags2).difference(tags1)))
+
+print(transition_evaluation())
 
 
